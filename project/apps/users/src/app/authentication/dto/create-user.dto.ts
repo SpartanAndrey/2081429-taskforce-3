d9c55234eaf11a1b9710149f72fd4da6be91fded
SpartanAndrey <superpinguin9@gmail.com>
2023-04-17 16:13:31 +0300

    Section 5.4 Users service validation

--- project/apps/users/src/app/authentication/dto/create-user.dto.ts
+++ project/apps/users/src/app/authentication/dto/create-user.dto.ts
@@ -1,17 +1,21 @@
 import { City, UserRole } from '@project/shared/app-types';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsISO8601, IsString } from 'class-validator';
+import { AUTH_USER_DATE_BIRTH_NOT_VALID, AUTH_USER_EMAIL_NOT_VALID } from '../authentication.constant';
 
 export class CreateUserDto {
   @ApiProperty({
     description: 'Фамилия и имя пользователя. Минимальная длина поля: 3 символа, максимальная 50 символов.',
     example: 'Ivan Ivanov'
   })
+  @IsString()
   public fullName: string;
 
   @ApiProperty({
     description: 'Используется в качестве логина. Адрес уникален: в базе данных не может быть двух пользователей с одинаковым email.',
     example: 'user@gmail.com'
   })
+  @IsEmail({}, { message: AUTH_USER_EMAIL_NOT_VALID })
   public email: string;
 
   @ApiProperty({
@@ -24,6 +28,7 @@ export class CreateUserDto {
     description: 'Пароль пользователя. Минимальная длина пароля 6 символов, максимальная 12.',
     example: '123456'
   })
+  @IsString()
   public password: string;
 
   @ApiProperty({
@@ -42,5 +47,6 @@ export class CreateUserDto {
     description: 'Дата рождения пользователя в формате: YYYY-MM-DD. Ограничения: валидная дата, пользователь достиг совершеннолетия (18 лет).',
     example: '2022-02-22'
   })
+  @IsISO8601({}, { message: AUTH_USER_DATE_BIRTH_NOT_VALID })
   public dateBirth: Date;
 }
